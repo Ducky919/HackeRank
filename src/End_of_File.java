9f1a94707769118fe7373f00d22a001de0dc7867
Ducky919 <ducky_the_legend96@yahoo.com>
2020-07-15 17:14:08 +0900

    Hackerank Introduction Completed

---
+++ src/End_of_File.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class End_of_File {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s;
+        int count = 0;
+        while (scan.hasNext()) {
+            s = scan.nextLine();
+            count ++;
+            System.out.printf("%d %s%n",count,s);
+
+        }
+
+    }
+}
