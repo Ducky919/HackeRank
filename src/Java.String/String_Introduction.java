00f2ea15ae4c0717250a92e48d9920c5671750a2
Ducky919 <ducky_the_legend96@yahoo.com>
2020-07-16 16:31:36 +0900

    Change String_Introduction to new version

--- src/Java.String/String_Introduction.java
+++ src/Java.String/String_Introduction.java
@@ -4,28 +4,35 @@ import java.util.*;
 import java.util.Scanner;
 
 public class String_Introduction {
-
+    static void NameLength(String FirstName,String LastName) {
+        int LengthFirstName=FirstName.length();
+        int LengthLastName=LastName.length();
+        System.out.println(LengthFirstName + LengthLastName);
+    }
+    static void NameCompare(String FirstName,String LastName){
+        if (FirstName.compareTo(LastName) > 0) {
+            System.out.println(FirstName);
+        } else if (FirstName.compareTo(LastName) < 0) {
+            System.out.println(LastName);
+        } else {
+            System.out.println("FirstName and LastName are Equal");
+        }
+    }
+    static void FullName(String FirstName,String LastName) {
+        String UpperFirstName=FirstName.substring(0,1).toUpperCase() + FirstName.substring(1);
+        String UpperLastName=LastName.substring(0,1).toUpperCase() + LastName.substring(1);
+        String FullName=UpperFirstName + " " + UpperLastName;
+        System.out.println(FullName);
+    }
     public static void main(String[] args) {
 
         Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        String B=sc.next();
+        String FirstName=sc.next();
+        String LastName=sc.next();
 
-        int LenA = A.length();
-        int LenB = B.length();
-        System.out.println(LenA + LenB);
-
-        if (A.compareTo(B) > 0) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
-
-        String UpperA = A.substring(0,1).toUpperCase() + A.substring(1);
-        String UpperB = B.substring(0,1).toUpperCase() + B.substring(1);
-
-        System.out.println(UpperA + " " + UpperB);
-
+        NameLength(FirstName,LastName);
+        NameCompare(FirstName,LastName);
+        FullName(FirstName,LastName);
     }
 }
 
