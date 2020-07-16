1a71a24b04c1ae07fb11704c6848e98b09126030
Luong Hoang Viet <ducky_the_legend96@yahoo.com>
2020-07-16 18:49:03 +0900

    Update syntax

--- src/Java.String/StringIntroduction.java
+++ src/Java.String/StringIntroduction.java
@@ -4,35 +4,35 @@ import java.util.*;
 import java.util.Scanner;
 
 public class StringIntroduction {
-    static void nameLength(String _firstName,String _lastName) {
-        int lengthFirstName = _firstName.length();
-        int lengthLastName = _lastName.length();
+    static void caculateNameLength(String firstName, String lastName) {
+        int lengthFirstName = firstName.length();
+        int lengthLastName  = lastName.length();
         System.out.println(lengthFirstName + lengthLastName);
     }
-    static void nameCompare(String _firstName,String _lastName){
-        if (_firstName.compareTo(_lastName) > 0) {
+    static void checkName(String firstName, String lastName){
+        if (firstName.compareTo(lastName) > 0) {
             System.out.println("First Name is longer than Last Name");
-        } else if (_firstName.compareTo(_lastName) < 0) {
+        } else if (firstName.compareTo(lastName) < 0) {
             System.out.println("Last Name is longer than First Name");
         } else {
             System.out.println("FirstName and LastName are Equal");
         }
     }
-    static void fullName(String _firstName,String _lastName) {
-        String upperFirstName = _firstName.substring(0,1).toUpperCase() + _firstName.substring(1);
-        String upperLastName = _lastName.substring(0,1).toUpperCase() + _lastName.substring(1);
-        String fullName = upperFirstName + " " + upperLastName;
+    static void showFullName(String firstName, String lastName) {
+        String upperFirstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1);
+        String upperLastName  = lastName.substring(0, 1).toUpperCase() + lastName.substring(1);
+        String fullName       = upperFirstName + " " + upperLastName;
         System.out.println(fullName);
     }
     public static void main(String[] args) {
 
         Scanner sc=new Scanner(System.in);
         String firstName = sc.next();
-        String lastName = sc.next();
+        String lastName  = sc.next();
 
-        nameLength(firstName,lastName);
-        nameCompare(firstName,lastName);
-        fullName(firstName,lastName);
+        caculateNameLength(firstName, lastName);
+        checkName(firstName, lastName);
+        showFullName(firstName, lastName);
     }
 }
 
