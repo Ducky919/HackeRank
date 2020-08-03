a81e8017d53290971621ee4fe12ba8b7cd9fd136
Ducky919 <ducky_the_legend96@yahoo.com>
2020-08-03 17:41:34 +0900

    Add Java Handling Chapter

---
+++ src/Java.String/JavaHandlingTryCatch.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+public class JavaHandlingTryCatch {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            int x = sc.nextInt(); // e
+            int y = sc.nextInt(); // 1
+            float result;
+            result = (float)x/y;
+            System.out.println(result);
+        }  catch (InputMismatchException e) {
+            System.out.println("java.util.InputMismatchException");
+        }
+        catch (ArithmeticException e) {
+            System.out.println("java.lang.ArithmeticException: / by zero");
+        }
+    }
+}
